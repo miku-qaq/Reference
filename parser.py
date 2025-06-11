b08cafcf2664b545a8bf4f37fc3d1badf52b0707
miku-qaq <1013418280@qq.com>
2025-06-11 22:56:47 +0800

    2

--- parser.py
+++ parser.py
@@ -141,3 +141,4 @@ def parse_references_bulk(text_block: str) -> List[Reference]:
             results.append(parse_reference(line))
         except Exception:
             results.append(Reference(line))
+
